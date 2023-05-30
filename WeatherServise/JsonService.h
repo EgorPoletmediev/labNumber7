57a79dc7b6917ef0150910fffd0039a914cb9623
Egor Poletaev <egor.poletaev.kirov@gmail.com>
2023-05-30 09:59:43 +0300

    added JsonServise class

---
+++ WeatherServise/JsonService.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "Weather.h"
+#include "Service.h"
+#include "json.hpp"
+
+using nlohmann::json;
+
+class JsonService :public Service
+{
+public:
+	virtual Weather getWeather(std::string s) override;
+	virtual ~JsonService() {};
+};
+
+
